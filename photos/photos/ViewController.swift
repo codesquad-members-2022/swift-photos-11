7bd54e722445f9bf1753ddecaca091c7b24fe7d3
OceanShape <oceanshape700@gmail.com>
2022-03-21 17:27:31 +0900

    feat: UICollectionView와 cell추가

--- photos/photos/ViewController.swift
+++ photos/photos/ViewController.swift
@@ -7,7 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    let cellCount = 40
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
