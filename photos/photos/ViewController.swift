0dd00ac6ea02e016c03cd4b27d38a418a72471f1
Jinsujin <sylpid003@gmail.com>
2022-03-25 16:32:45 +0900

    ref: ViewController 리펙토링 (#2)

--- photos/photos/ViewController.swift
+++ photos/photos/ViewController.swift
@@ -8,54 +8,41 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    let cellCount = 40
-    var currentIndex = 0
-    
+
+class ViewController: UIViewController {
+    private var currentIndex = 0
     private var allPhotos: PHFetchResult<PHAsset>?
-    var thumbnailSize = CGSize.zero
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cellCount
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! AlbumCell
-        
-        let asset: PHAsset?
-        if self.currentIndex >= 6 {
-            asset = self.allPhotos?[0]
-        } else {
-            asset = self.allPhotos?[indexPath.item]
-        }
-        currentIndex += 1
-        LocalImageManager.shared.requestImage(with: asset, thumbnailSize: self.thumbnailSize) {
-            (image) in cell.configure(with: image)
-        }
-//        let r = CGFloat.random(in: 0...1)
-//        let g = CGFloat.random(in: 0...1)
-//        let b = CGFloat.random(in: 0...1)
-//        let backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
-//        cell.contentView.backgroundColor = backgroundColor
-        
-        return cell
-    }
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Photos"
-        
         self.allPhotos = PHAsset.fetchAssets(with: nil)
         self.collectionView.reloadData()
-        self.thumbnailSize = CGSize(width: 100.0, height: 100.0)
-        //let asset = self.allPhotos?[indexPath.item]
     }
 }
 
+// MARK:- UICollecitonView Delegate
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allPhotos?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! AlbumCell
+        let asset = self.allPhotos?[indexPath.item]
+        let thumbnailSize = CGSize(width: 100.0, height: 100.0)
+        LocalImageManager.shared.requestImage(with: asset, thumbnailSize: thumbnailSize) {
+            (image) in cell.configure(with: image)
+        }
+        return cell
+    }
+}
+
+
+// MARK:- PHPhotoLibraryChangeObserver
 extension ViewController: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
         self.allPhotos = PHAsset.fetchAssets(with: nil)
